5fc3a09efb0da6d8a2372ff8a9817f58fe140382
Pieczasz <bartekp854@gmail.com>
2024-12-07 13:49:17 +0100

    Wrap AboutMeHero, Education, and Internships components in MaxWidthWrapper for improved layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,16 @@
 import AboutMeHero from "@/components/AboutMeHero";
 import Education from "@/components/Education";
 import Internships from "@/components/Internships";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default async function Home() {
   return (
     <div className="container mx-auto px-2 md:px-4">
-      <AboutMeHero />
-      <Education />
-      <Internships />
+      <MaxWidthWrapper>
+        <AboutMeHero />
+        <Education />
+        <Internships />
+      </MaxWidthWrapper>
     </div>
   );
 }
