e360e4f5925391af649cdbadc03bcbe0b2bfc111
Pieczasz <bartekp854@gmail.com>
2025-01-28 10:09:28 +0100

    Remove unused import of Lock icon in ZabrzeFranciszekPage component

--- src/app/projects/zabrze-franciszek/page.tsx
+++ src/app/projects/zabrze-franciszek/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
 import Link from "next/link";
-import { Github, Link2, X, Lock } from "lucide-react";
+import { Github, Link2, X } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   SiTypescript,
